f02a023c0db804f5f8f453fd294bf6bfc48c0a0f
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-06-21 22:34:51 +0700

    🤓

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -1,4 +1,5 @@
 package animateatlas;
+
 import flixel.util.FlxDestroyUtil;
 import openfl.geom.Rectangle;
 import flixel.math.FlxPoint;
@@ -16,9 +17,6 @@ import flixel.graphics.frames.FlxFrame;
 #if desktop
 import sys.FileSystem;
 import sys.io.File;
-#else
-import js.html.FileSystem;
-import js.html.File;
 #end
 
 using StringTools;
